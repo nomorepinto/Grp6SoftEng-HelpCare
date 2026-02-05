96ad7295aa09db0c5e8e102395b5b10d003d05c5
Jedrick <bilogdino071@gmail.com>
2026-02-05 16:06:01 +0800

    changed prompt

--- src/components/functions/geminiFunctions.ts
+++ src/components/functions/geminiFunctions.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${process.env.EXPO_PUBLIC_GEMINI_API_KEY}`;
+
 
 
 export const testGemini = async () => {
@@ -26,12 +28,12 @@ export const testGemini = async () => {
 
 export const askGemini = async (dataBase64: string) => {
     try {
-
+        const prompt = "Analyze the image. Return a JSON that will satisfy the following type: \n\n\ntype Medicine = {\n    name: string;\n    dose: string;\n    frequency: string;\n    duration: string;\n}; if the identified image is not a prescription, set the name to 'not a prescription' and all other values blank";
         const requestBody = {
             contents: [{
                 role: "user",
                 parts: [
-                    { text: "Analyze the image. Return medicine name, dose, frequency, and duration." },
+                    { text: prompt },
                     {
                         inlineData: {
                             data: dataBase64, // Ensure this is just the base64 string, no "data:image/jpeg;base64," prefix
@@ -45,6 +47,7 @@ export const askGemini = async (dataBase64: string) => {
         const response = await axios.post(url, requestBody);
 
         console.log(response.data.candidates[0].content.parts[0].text);
+
     } catch (error: any) {
         // Axios catches 4xx and 5xx errors automatically
         console.error("Status:", error.response?.status);
